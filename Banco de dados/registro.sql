bb9847c84a1d561ae9d42f04aa3d14f43a039fcb
TonhaoGameplays <128548364+TonhaoGameplays@users.noreply.github.com>
2023-03-23 20:15:28 -0300

    Add files via upload

--- Banco de dados/registro.sql
+++ Banco de dados/registro.sql
@@ -1,23 +1,6 @@
--- --------------------------------------------------------
--- Servidor:                     127.0.0.1
--- Versão do servidor:           8.0.26 - MySQL Community Server - GPL
--- OS do Servidor:               Win64
--- HeidiSQL Versão:              11.2.0.6213
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
--- Copiando estrutura do banco de dados para registro
-CREATE DATABASE IF NOT EXISTS `registro` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE IF NOT EXISTS `registro`;
 USE `registro`;
 
--- Copiando estrutura para tabela registro.dados
 CREATE TABLE IF NOT EXISTS `dados` (
   `ID` int NOT NULL AUTO_INCREMENT,
   `nomeusuario` varchar(50) NOT NULL,
@@ -25,16 +8,12 @@ CREATE TABLE IF NOT EXISTS `dados` (
   `email` varchar(50) NOT NULL,
   `avatar` varchar(50) NOT NULL,
   `tipo_de_usuario` varchar(50) NOT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+	PRIMARY KEY (ID)
+	);
 
--- Copiando dados para a tabela registro.dados: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `dados` DISABLE KEYS */;
 REPLACE INTO `dados` (`ID`, `nomeusuario`, `senha`, `email`, `avatar`, `tipo_de_usuario`) VALUES
 	(1, 'anthony', '123456', 'abc@hotmail.com', 'ETECJA_Logo.png', 'Usuario');
-/*!40000 ALTER TABLE `dados` ENABLE KEYS */;
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+INSERT INTO `dados` (`ID`, `nomeusuario`, `senha`, `email`, `avatar`, `tipo_de_usuario`) VALUES
+	(2, 'anthony', '12345678', 'anthony@hotmail.com', 'ETECJA_Logo.png', 'Usuario');
+
